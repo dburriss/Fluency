ebede088905e750b20096e48edb3aaf2c02ea5a6
dburriss <devon@chimplab.co>
2018-02-28 07:16:51 +0100

    Adds Variable class

---
+++ src/Fluency/Conventions/Variable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace Fluency.Conventions
+{
+    public class Variable
+    {
+        public string Name { get; }
+        public Type Type { get; }
+        public object DefaultValue { get; }
+
+        private Variable(string name, Type type, object defaultValue = null)
+        {
+            Name = name;
+            Type = type;
+            if (defaultValue == DBNull.Value)
+            {
+                defaultValue = null;
+            }
+            if (defaultValue == null && type.IsValueType)
+            {
+                defaultValue = Activator.CreateInstance(type);
+            }
+            DefaultValue = defaultValue;
+        }
+
+        internal static Variable From(PropertyInfo propertyInfo)
+        {
+            return new Variable(propertyInfo.Name, propertyInfo.PropertyType);
+        }
+
+        internal static Variable From(ParameterInfo parameterInfo)
+        {
+            return new Variable(parameterInfo.Name, parameterInfo.ParameterType, parameterInfo.DefaultValue);
+        }
+    }
+}
